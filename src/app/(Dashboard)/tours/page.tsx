93baf670ba76bd4bbf857fc5439cc16764222bea
aka <aaronkaltman@gmail.com>
2023-12-20 18:49:21 -0500

    adding dashboard pages and layout

---
+++ src/app/(Dashboard)/tours/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function TourPage() {
+  return (
+    <div>
+      <section>
+        <h1>Tour Page</h1>
+      </section>
+    </div>
+  );
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,8 @@ export default function HomePageage() {
   return (
     <div>
       <h1 className="text-3xl">HomePageage</h1>
-      <GetData />
+      <p>lorem ipsum stuff</p>
+      <button>Get Started</button>
     </div>
   );
 }
